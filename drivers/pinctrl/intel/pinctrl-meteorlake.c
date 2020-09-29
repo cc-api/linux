9d4ea537480a29f068c2abb29a97a4518f50cad4
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2020-09-29 13:57:29 +0300

    TO BE FOLDED: pinctrl: meteorlake: Add ACPI IDs as per v40
    Add ACPI IDs as per v40 of 'Master List of ACPI IDs'.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- drivers/pinctrl/intel/pinctrl-meteorlake.c
+++ drivers/pinctrl/intel/pinctrl-meteorlake.c
@@ -459,7 +459,7 @@ static const struct intel_pinctrl_soc_data *mtl_soc_data_array[] = {
 };
 
 static const struct acpi_device_id mtl_pinctrl_acpi_match[] = {
-	{ "", (kernel_ulong_t)mtl_soc_data_array },
+	{ "INTC1083", (kernel_ulong_t)mtl_soc_data_array },
 	{ }
 };
 MODULE_DEVICE_TABLE(acpi, mtl_pinctrl_acpi_match);
