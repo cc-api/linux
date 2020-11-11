5526e41ceb4d2bfa2d233e67c4d9f323eecbe115
Zhuocheng Ding <zhuocheng.ding@intel.com>
2020-11-11 10:54:48 +0800

    thermal: intel: hfi: Split some HFI data structures into a header file
    The virtualization of HFI/ITD needs access to the data structures
    describing the HFI table and HFI CPUID.
    
    Place them in a separate header file.
    
    Signed-off-by: Zhuocheng Ding <zhuocheng.ding@intel.com>
    Co-developed-by: Zhao Liu <zhao1.liu@intel.com>
    Signed-off-by: Zhao Liu <zhao1.liu@intel.com>
    ---
    Changes since v3:
     * None.
    
    Changes since v2:
     * Remove the Suggested-by tag about changlog rewriting. (Ricardo)
    
    Changes since v1:
     * Rewrite the commit message by stealing Ricardo's expression.
       (Ricardo)
     * Split the hfi_table structure related change into a new commit.
       (Ricardo)

--- drivers/thermal/intel/intel_hfi.c
+++ drivers/thermal/intel/intel_hfi.c
@@ -48,6 +48,7 @@
 #include <linux/workqueue.h>
 
 #include <asm/intel-family.h>
+#include <asm/hfi.h>
 #include <asm/msr.h>
 
 #ifdef CONFIG_DEBUG_FS
@@ -62,37 +63,7 @@
 
 #include "../thermal_netlink.h"
 
-/* CPUID detection and enumeration definitions for HFI */
-
 #define CPUID_HFI_LEAF 6
-
-union hfi_capabilities {
-	struct {
-		u8	performance:1;
-		u8	energy_efficiency:1;
-		u8	__reserved:6;
-	} split;
-	u8 bits;
-};
-
-union cpuid6_edx {
-	struct {
-		union hfi_capabilities	capabilities;
-		u32			table_pages:4;
-		u32			__reserved:4;
-		s32			index:16;
-	} split;
-	u32 full;
-};
-
-union cpuid6_ecx {
-	struct {
-		u32	dont_care0:8;
-		u32	nr_classes:8;
-		u32	dont_care1:16;
-	} split;
-	u32 full;
-};
 
 union hfi_thread_feedback_char_msr {
 	struct {
@@ -117,36 +88,6 @@ struct hfi_cpu_data {
 } __packed;
 
 /**
- * struct hfi_hdr - Header of the HFI table
- * @perf_updated:	Hardware updated performance capabilities
- * @ee_updated:		Hardware updated energy efficiency capabilities
- *
- * Properties of the data in an HFI table. There exists one header per each
- * HFI class.
- */
-struct hfi_hdr {
-	u8	perf_updated;
-	u8	ee_updated;
-} __packed;
-
-/**
- * struct hfi_table - Representation of an HFI table
- * @base_addr:		Base address of the local copy of the HFI table
- * @timestamp:		Timestamp of the last update of the local table.
- *			Located at the base of the local table.
- * @hdr:		Base address of the header of the local table
- * @data:		Base address of the data of the local table
- */
-struct hfi_table {
-	union {
-		void			*base_addr;
-		u64			*timestamp;
-	};
-	void			*hdr;
-	void			*data;
-};
-
-/**
  * struct hfi_instance - Representation of an HFI instance (i.e., a table)
  * @local_table:	Local copy of HFI table for this instance
  * @cpus:		CPUs represented in this HFI table instance
@@ -168,26 +109,6 @@ struct hfi_instance {
 	struct hfi_hdr		*cap_upd_hist;
 	unsigned int		cap_upd_hist_idx;
 #endif
-};
-
-/**
- * struct hfi_features - Supported HFI features
- * @nr_classes:		Number of classes supported
- * @nr_table_pages:	Size of the HFI table in 4KB pages
- * @cpu_stride:		Stride size to locate the capability data of a logical
- *			processor within the table (i.e., row stride)
- * @class_stride:	Stride size to locate a class within the capability
- *			data of a logical processor or the HFI table header
- * @hdr_size:		Size of the table header
- *
- * Parameters and supported features that are common to all HFI instances
- */
-struct hfi_features {
-	unsigned int	nr_classes;
-	size_t		nr_table_pages;
-	unsigned int	cpu_stride;
-	unsigned int	class_stride;
-	unsigned int	hdr_size;
 };
 
 /**
