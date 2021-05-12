d773e42c008581fa5e737bd3391d3a7a73aa90b5
Tomas Winkler <tomas.winkler@intel.com>
2021-05-12 11:50:32 +0300

    INTEL_DII: mei: mkhi: add memory ready command
    Add GSC memory ready command. The command  indicates to the firmware that
    extend operation memory was setup and the firmware may enter PXP mode.
    
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>
    Reviewed-by: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>

--- drivers/misc/mei/mkhi.h
+++ drivers/misc/mei/mkhi.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
- * Copyright (c) 2003-2020, Intel Corporation. All rights reserved.
+ * Copyright (c) 2003-2021, Intel Corporation. All rights reserved.
  * Intel Management Engine Interface (Intel MEI) Linux driver
  */
 
@@ -17,6 +17,13 @@
 #define MKHI_GEN_GET_FW_VERSION_CMD 0x2
 
 #define MCHI_GROUP_ID  0xA
+
+#define MKHI_GROUP_ID_GFX              0x30
+#define MKHI_GFX_RESET_WARN_CMD_REQ    0x0
+#define MKHI_GFX_MEMORY_READY_CMD_REQ  0x1
+
+/* Allow transition to PXP mode without approval */
+#define MKHI_GFX_MEM_READY_PXP_ALLOWED  0x1
 
 struct mkhi_rule_id {
 	__le16 rule_type;
@@ -40,6 +47,11 @@ struct mkhi_msg_hdr {
 struct mkhi_msg {
 	struct mkhi_msg_hdr hdr;
 	u8 data[];
+} __packed;
+
+struct mkhi_gfx_mem_ready {
+	struct mkhi_msg_hdr hdr;
+	uint32_t flags;
 } __packed;
 
 #endif /* _MEI_MKHI_H_ */
