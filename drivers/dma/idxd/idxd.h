43bd675728ae5a4ecce5abeb6459ccb7a2e67de2
Dave Jiang <dave.jiang@intel.com>
2021-09-28 16:34:00 -0700

    dmaengine: idxd: add support for inter-pasid table create window ioctl
    Add ioctl function that creates an inter-domain pasid window for operation.
    The "owner" is defined as the application that is opening a memory window
    to allow another process to DMA into the owner's region. It sends the
    create_window ioctl in order to create an IDPT entry. The owner can specify
    whether the entry type should be type 0 (single submitter) or type 1
    (multiple submitters). The driver will return a file handle to this entry.
    The IDPT entry at this point has the valid bit set to 0. The ioctl also
    returns a 16bit handle to the IDPT entry. This handle is used as a field in
    the "window update" descriptor usable by the owner to update the window
    parameters.
    
    When the submitter is done with operations, it can close the IDPT entry
    file handle. This will trigger the draining of the submitter pasid.
    
    When the owner is done it will also close its IDPT entry file handle. This
    effectively deletes the window and removes the IDPT entry.
    
    Signed-off-by: Dave Jiang <dave.jiang@intel.com>

--- drivers/dma/idxd/idxd.h
+++ drivers/dma/idxd/idxd.h
@@ -319,6 +319,18 @@ struct vdev_device_ops {
 	int (*device_remove)(struct idxd_device *idxd, char *vdev_name);
 };
 
+struct idxd_idpt_entry_data {
+	struct files_struct *owner_id;
+	struct list_head submit_list;
+	struct mutex lock;
+	u16 handle;
+	bool handle_valid;
+	struct idxd_device *idxd;
+	struct list_head next;
+	u32 access_pasid;
+	struct iommu_sva *owner_sva;
+};
+
 struct idxd_device {
 	struct idxd_dev idxd_dev;
 	struct idxd_driver_data *data;
@@ -371,6 +383,9 @@ struct idxd_device {
 
 	unsigned int idpt_size;
 	unsigned int idpte_support_mask;
+	struct ida idpt_ida;
+	struct mutex idpt_lock;
+	struct idxd_idpt_entry_data **idpte_data;
 
 	union sw_err_reg sw_err;
 	wait_queue_head_t cmd_waitq;
