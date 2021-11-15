6023ac54d71588bbec312504f5463b95693fdae6
Ira Weiny <ira.weiny@intel.com>
2021-11-15 12:35:20 -0800

    mm/pkeys: Introduce pks_mk_readwrite()
    When a user needs valid access to a PKS protected page they will need to
    change the protections for their pkey to Read/Write within a thread of
    execution.
    
    Define pks_mk_readwrite() to update the specified Pkey.  Define
    pks_update_protection() as a helper to do the heavy lifting and to allow
    for subsequent pks_mk_*() calls.  Define PKEY_READ_WRITE rather than use
    a magic value of '0' in pks_update_protection().  Finally, ensure
    preemption is disabled while calling pks_write_pkrs() in this code.
    
    Add documentation.
    
    Signed-off-by: Ira Weiny <ira.weiny@intel.com>
    
    ---
    Changes for V8
    	Define PKEY_READ_WRITE
    	Make the call inline
    	Clean up the names
    	Use pks_write_pkrs() with preemption disabled
    	Split this out from 'Add PKS kernel API'
    	Include documentation in this patch

--- include/linux/pkeys.h
+++ include/linux/pkeys.h
@@ -48,6 +48,31 @@ static inline bool arch_pkeys_enabled(void)
 
 #endif /* ! CONFIG_ARCH_HAS_PKEYS */
 
+#ifdef CONFIG_ARCH_ENABLE_SUPERVISOR_PKEYS
+
 #include <linux/pks-keys.h>
+#include <linux/types.h>
+
+#include <uapi/asm-generic/mman-common.h>
+
+void pks_update_protection(int pkey, u32 protection);
+
+/**
+ * pks_mk_readwrite() - Make the domain Read/Write
+ * @pkey: the pkey for which the access should change.
+ *
+ * Allow all access, read and write, to the domain specified by pkey.  This is
+ * not a global update and only affects the current running thread.
+ */
+static inline void pks_mk_readwrite(int pkey)
+{
+	pks_update_protection(pkey, PKEY_READ_WRITE);
+}
+
+#else /* !CONFIG_ARCH_ENABLE_SUPERVISOR_PKEYS */
+
+static inline void pks_mk_readwrite(int pkey) {}
+
+#endif /* CONFIG_ARCH_ENABLE_SUPERVISOR_PKEYS */
 
 #endif /* _LINUX_PKEYS_H */
