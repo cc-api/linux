3f56ff77ad7350e5c0faedff935ff4de4633d040
Jiangbo Wu <jiangbo.wu@intel.com>
2022-01-18 10:25:49 -0700

    dmaengine: idxd: Add batch descriptor support
    The DSA device can support a group of descriptors for submission at once.
    The DSA spec calls this operation a 'batch' operation. The batch descriptor
    points to an array of descriptors where the beginning of the address list
    is 64-byte aligned.
    
    Signed-off-by: Jiangbo Wu <jiangbo.wu@intel.com>
    Signed-off-by: Dave Jiang <dave.jiang@intel.com>

--- drivers/dma/idxd/idxd.h
+++ drivers/dma/idxd/idxd.h
@@ -322,6 +322,23 @@ struct idxd_device {
 	unsigned long *opcap_bmap;
 };
 
+/*
+ * IDXD batch field for SW Batch descriptor
+ * @descs: Descriptor list address
+ * @dma_descs: DMA address for descs
+ * @cr: completion record list address
+ * @dma_cr: DMA address for completion records
+ * @num: Number of descs in batch
+ */
+struct idxd_batch {
+	struct dsa_hw_desc *descs;
+	dma_addr_t dma_descs;
+	struct dsa_completion_record *crs;
+	dma_addr_t dma_crs;
+	u32 num;
+	u32 max;
+};
+
 /* IDXD software descriptor */
 struct idxd_desc {
 	union {
@@ -341,6 +358,8 @@ struct idxd_desc {
 	u16 gen;
 	int cpu;
 	struct idxd_wq *wq;
+
+	struct idxd_batch *batch;
 };
 
 /*
