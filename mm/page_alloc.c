2d320532502e63890ce3238482aef524caa35b85
Tony Luck <tony.luck@intel.com>
2022-01-28 17:10:28 +0800

    mm/pagezero: Pass pointer to gfp_mask to functions called from get_page_from_freelist()
    When code is added that can allocate a pre-zeroed page it will be
    necessary to clear the __GFP_ZERO bit to make sure the page isn't
    cleared again by kernel_init_free_pages()
    
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -3649,7 +3649,7 @@ struct page *__rmqueue_pcplist(struct zone *zone, unsigned int order,
 /* Lock and remove page from the per-cpu list */
 static struct page *rmqueue_pcplist(struct zone *preferred_zone,
 			struct zone *zone, unsigned int order,
-			gfp_t gfp_flags, int migratetype,
+			gfp_t *gfp_flags, int migratetype,
 			unsigned int alloc_flags)
 {
 	struct per_cpu_pages *pcp;
@@ -3682,7 +3682,7 @@ static struct page *rmqueue_pcplist(struct zone *preferred_zone,
 static inline
 struct page *rmqueue(struct zone *preferred_zone,
 			struct zone *zone, unsigned int order,
-			gfp_t gfp_flags, unsigned int alloc_flags,
+			gfp_t *gfp_flags, unsigned int alloc_flags,
 			int migratetype)
 {
 	unsigned long flags;
@@ -3705,7 +3705,7 @@ struct page *rmqueue(struct zone *preferred_zone,
 	 * We most definitely don't want callers attempting to
 	 * allocate greater than order-1 page units with __GFP_NOFAIL.
 	 */
-	WARN_ON_ONCE((gfp_flags & __GFP_NOFAIL) && (order > 1));
+	WARN_ON_ONCE((*gfp_flags & __GFP_NOFAIL) && (order > 1));
 	spin_lock_irqsave(&zone->lock, flags);
 
 	do {
@@ -4160,7 +4160,7 @@ retry:
 
 try_this_zone:
 		page = rmqueue(ac->preferred_zoneref->zone, zone, order,
-				gfp_mask, alloc_flags, ac->migratetype);
+				&gfp_mask, alloc_flags, ac->migratetype);
 		if (page) {
 			prep_new_page(page, order, gfp_mask, alloc_flags);
 
