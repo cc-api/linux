d90d112a047083cb16ec3d15ef94ff1f480682e7
Isaku Yamahata <isaku.yamahata@intel.com>
2022-02-11 11:51:38 -0800

    fixup! KVM: TDX: Add helper function to initialize TDX module

--- arch/x86/kvm/vmx/tdx_stubs.c
+++ arch/x86/kvm/vmx/tdx_stubs.c
@@ -6,7 +6,6 @@
 void __init tdx_pre_kvm_init(unsigned int *vcpu_size,
 			unsigned int *vcpu_align, unsigned int *vm_size) {}
 
-int tdx_module_setup(void) { return -EOPNOTSUPP; };
 int __init tdx_hardware_setup(struct kvm_x86_ops *x86_ops) { return -EOPNOTSUPP; }
 void tdx_hardware_unsetup(void) {}
 void tdx_hardware_enable(void) {}
