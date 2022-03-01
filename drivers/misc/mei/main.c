ec613195087e001b7e5946a0f73aa7b14c472845
Alexander Usyskin <alexander.usyskin@intel.com>
2022-03-01 16:36:25 +0200

    INTEL_DII: mei: avoid reset if fw is down
    If FW is not ready and FW reset flag is off
    do not start reset flow as FW will not answer anyway.
    Wait till interrupt with FW reset flag on to start reset flow.
    
    Flow:
    FW on the way down unset ME_RDY_HRA and ME_RST_HRA and sends interrupt.
    Driver receives interrupt and stops all communication.
    (Before the patch here the driver starts reset flow).
    FW on the way up set ME_RST_HRA and sends interrupt.
    Driver receives interrupt and starts the link reset flow.
    FW sets ME_RDY_HRA and unsets ME_RST_HRA and sends interrupt.
    Driver continues with link reset flow.
    
    Limit the flow to GSC for now.
    
    Signed-off-by: Alexander Usyskin <alexander.usyskin@intel.com>

--- drivers/misc/mei/main.c
+++ drivers/misc/mei/main.c
@@ -1121,6 +1121,7 @@ void mei_set_devstate(struct mei_device *dev, enum mei_dev_state state)
 		put_device(clsdev);
 	}
 }
+EXPORT_SYMBOL_GPL(mei_set_devstate);
 
 /**
  * kind_show - display device kind
