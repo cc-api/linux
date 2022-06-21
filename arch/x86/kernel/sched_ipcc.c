02e9decc9a3fb6739b1243fba1bbc3c87c87936b
Ricardo Neri <ricardo.neri-calderon@linux.intel.com>
2022-06-21 16:52:44 -0700

    x86/sched/ipcc: Implement model-specific checks for task classification
    In Alder Lake and Raptor Lake, the result of thread classification is more
    accurate when only one SMT sibling is busy. Classification results for
    class 2 and 3 are always reliable.
    
    Changing the classification of a task too frequently may lead to
    unnecessary migrations.
    
    Only update the class of a task if it is considered accurate and has been
    constant during four consecutive user ticks.
    
    Architectures define the meaning of task_struct::ipcc. Create a structure
    to keep the data needed to debounce the classification from hardware and
    the IPC class that the scheduler will use.
    
    Cc: Ben Segall <bsegall@google.com>
    Cc: Daniel Bristot de Oliveira <bristot@redhat.com>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Ionela Voinescu <ionela.voinescu@arm.com>
    Cc: Joel Fernandes (Google) <joel@joelfernandes.org>
    Cc: Len Brown <len.brown@intel.com>
    Cc: Lukasz Luba <lukasz.luba@arm.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Perry Yuan <Perry.Yuan@amd.com>
    Cc: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Cc: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Tim C. Chen <tim.c.chen@intel.com>
    Cc: Valentin Schneider <vschneid@redhat.com>
    Cc: Zhao Liu <zhao1.liu@linux.intel.com>
    Cc: x86@kernel.org
    Cc: linux-pm@vger.kernel.org
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Ricardo Neri <ricardo.neri-calderon@linux.intel.com>
    ---
    Changes since v4:
     * Partition task_struct::ipcc to store the data that the classification
       debouncer needs. This hides this arch-specific details from the
       scheduler. (Ionela).
    
    Changes since v3:
     * Relocated this code to arch/x86/kernel/sched_ipcc.c (Rafael)
    
    Changes since v2:
     * None
    
    Changes since v1:
     * Adjusted the result the classification of Intel Thread Director to start
       at class 1. Class 0 for the scheduler means that the task is
       unclassified.
     * Used the new names of the IPC classes members in task_struct.
     * Reworked helper functions to use sched_smt_siblings_idle() to query
       the idle state of the SMT siblings of a CPU.

--- arch/x86/kernel/sched_ipcc.c
+++ arch/x86/kernel/sched_ipcc.c
@@ -18,11 +18,85 @@
 
 #include <linux/sched.h>
 
+#include <asm/intel-family.h>
 #include <asm/topology.h>
+
+/**
+ * struct itd_ipcc - IPC class fields as used by Intel Thread Director
+ * @split.class:	The IPC class used for scheduling after filtering
+ *			hardware classification.
+ * @split.class_tmp:	Classification as read from hardware.
+ * @split.counter:	A counter to filter out temporary classifications.
+ * @full:		Full IPC class as carried in a task_struct.
+ */
+union itd_ipcc {
+	struct {
+		unsigned int	class:9;
+		unsigned int	class_tmp:9;
+		unsigned int	counter:14;
+	} split __packed;
+	unsigned int full;
+};
+
+#define CLASS_DEBOUNCER_SKIPS 4
+
+/**
+ * debounce_and_update_class() - Process and update a task's classification
+ *
+ * @p:		The task of which the classification will be updated
+ * @new_ipcc:	The new IPC classification
+ *
+ * Update the classification of @p with the new value that hardware provides.
+ * Only update the classification of @p if it has been the same during
+ * CLASS_DEBOUNCER_SKIPS consecutive ticks.
+ */
+static void debounce_and_update_class(struct task_struct *p, u8 new_ipcc)
+{
+	union itd_ipcc *itd_ipcc = (union itd_ipcc *)&p->ipcc;
+	u16 debounce_skip;
+
+	/* The class of @p changed. Only restart the debounce counter. */
+	if (itd_ipcc->split.class_tmp != new_ipcc) {
+		itd_ipcc->split.counter = 1;
+		goto out;
+	}
+
+	/*
+	 * The class of @p did not change. Update it if it has been the same
+	 * for CLASS_DEBOUNCER_SKIPS user ticks.
+	 */
+	debounce_skip = itd_ipcc->split.counter + 1;
+	if (debounce_skip < CLASS_DEBOUNCER_SKIPS)
+		itd_ipcc->split.counter++;
+	else
+		itd_ipcc->split.class = new_ipcc;
+
+out:
+	itd_ipcc->split.class_tmp = new_ipcc;
+}
+
+static bool classification_is_accurate(u8 hfi_class, bool smt_siblings_idle)
+{
+	switch (boot_cpu_data.x86_model) {
+	case INTEL_FAM6_ALDERLAKE:
+	case INTEL_FAM6_ALDERLAKE_L:
+	case INTEL_FAM6_RAPTORLAKE:
+	case INTEL_FAM6_RAPTORLAKE_P:
+	case INTEL_FAM6_RAPTORLAKE_S:
+		if (hfi_class == 3 || hfi_class == 2 || smt_siblings_idle)
+			return true;
+
+		return false;
+
+	default:
+		return false;
+	}
+}
 
 void intel_update_ipcc(struct task_struct *curr)
 {
 	u8 hfi_class;
+	bool idle;
 
 	if (intel_hfi_read_classid(&hfi_class))
 		return;
@@ -31,10 +105,16 @@ void intel_update_ipcc(struct task_struct *curr)
 	 * 0 is a valid classification for Intel Thread Director. A scheduler
 	 * IPCC class of 0 means that the task is unclassified. Adjust.
 	 */
-	curr->ipcc = hfi_class + 1;
+	idle = sched_smt_siblings_idle(task_cpu(curr));
+	if (classification_is_accurate(hfi_class, idle))
+		debounce_and_update_class(curr, hfi_class + 1);
 }
 
 long intel_get_ipcc_score(unsigned int ipcc, int cpu)
 {
-	return intel_hfi_get_ipcc_score(ipcc, cpu);
+	union itd_ipcc itd_ipcc;
+
+	itd_ipcc.full = ipcc;
+
+	return intel_hfi_get_ipcc_score(itd_ipcc.split.class, cpu);
 }
