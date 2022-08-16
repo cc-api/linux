750a4f8c976a778f7ba6c7ce3d159a1aec90cf80
Xu Yilun <yilun.xu@intel.com>
2022-08-16 15:04:33 +0800

    KVM: TDX: add a simple model for private iommu to support private dev
    Ideally the private iommu implementation should be in iommu driver, but
    it is WIP. So this patch uses a simple model for KVM to track:
    
    1. How many iommu devices is being used in KVM across TDs.
    2. How many iommu devices is being used by a TD.
    3. Which iommu device is the private device connected to.
    
    These info are useful when performing cache invalidation. Since the
    iommu Second Stage Paging is shared with EPT, so when the EPT changes,
    KVM should do iotlb invalidation for all affected iommus. Also a iommu
    device should avoid concurrent cache invalidation operations to ensure
    the correctness of its Invalidation Queue.
    
    Signed-off-by: Xu Yilun <yilun.xu@intel.com>

--- arch/x86/kvm/vmx/tdx.h
+++ arch/x86/kvm/vmx/tdx.h
@@ -51,6 +51,20 @@ struct tdx_binding_slot {
 	struct tdx_binding_slot_migtd migtd_data;
 };
 
+struct tdx_iommu {
+	u64 iommu_id;
+
+	struct kref ref;
+	struct list_head node;
+};
+
+struct kvm_tdx_iommu {
+	struct tdx_iommu *tiommu;
+	struct kref ref;
+
+	struct list_head node;
+};
+
 #define SERVTD_SLOTS_MAX 32
 struct kvm_tdx {
 	struct kvm kvm;
@@ -129,6 +143,7 @@ struct kvm_tdx {
 
 	/* mutex for tdi bind */
 	struct mutex ttdi_mutex;
+	struct list_head ktiommu_list;
 	struct list_head ttdi_list;
 
 	u64 mmio_offset;
@@ -459,6 +474,7 @@ struct tdx_tdi {
 	u16 rid;
 
 	struct kvm_tdx *kvm_tdx;
+	struct tdx_iommu *tiommu;
 	struct pci_tdi *tdi;
 
 	struct list_head node;
