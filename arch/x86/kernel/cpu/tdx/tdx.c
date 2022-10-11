intel/tdx

*** HACK *** KVM: TDX: Check if DEBUG_TRACE and NON_ARCH SEAMCALL available
DEBUG_TRACE related SEAMCALL and NON-ARCH SEAMCALL are Intel internal
debugging purpose SEAMCALLs. They are supported only with specific TDX
module builds.

Since there is no (architectural) way to determine if they are available or
not, try them unconditionally to detect the availability.

If DEBUG TRACE related SEAMCALL is not available, there's no need to
expose debug fs entries for configuring debug trace stuffs.

Originally-from: Isaku Yamahata <isaku.yamahata@intel.com>
Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>

arch/x86/kernel/cpu/tdx/tdx.c
@@ -110,6 +110,12 @@ bool is_tdx_module_enabled(void)
 }
 EXPORT_SYMBOL_GPL(is_tdx_module_enabled);	/* kvm_intel will use this. */
 
+bool is_debug_seamcall_available __ro_after_init = true;
+EXPORT_SYMBOL_GPL(is_debug_seamcall_available);
+
+bool is_nonarch_seamcall_available __ro_after_init = true;
+EXPORT_SYMBOL_GPL(is_nonarch_seamcall_available);
+
 /* TDX system information returned by TDH_SYS_INFO. */
 static struct tdsysinfo_struct *tdx_tdsysinfo;
 
@@ -864,14 +870,21 @@ static int __init __tdx_init_module(void)
 		goto out;
 
 	/*
-	 * Tracing is on by default, disable it before INITTDMR which causes
-	 * too many debug messages to take long time.
+	 * Detect if debug and non-arch seamcall available.
+	 *
+	 * Even though tracing level is ALL level by default, it needs to set
+	 * it explicitly to check if debug seamcall available.
 	 */
-	if (!trace_boot_seamcalls) {
-		ret = tdh_trace_seamcalls_boot(DEBUGCONFIG_TRACE_CUSTOM);
-		if (ret)
-			pr_err("failed to set trace level to DEBUGCONFIG_TRACE_CUSTOM.\n");
-	}
+	if (trace_boot_seamcalls)
+		tdh_trace_seamcalls_boot(DEBUGCONFIG_TRACE_ALL);
+	else
+		/*
+		 * Tracing is on by default, disable it before INITTDMR which
+		 * causes too many debug messages to take long time.
+		 */
+		tdh_trace_seamcalls_boot(DEBUGCONFIG_TRACE_CUSTOM);
+
+	tdxmode_boot(false, 0);
 
 	ret = tdx_init_tdmrs();
 out:

cc-api/linux

Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

arch/x86/kernel/cpu/tdx/tdx.c
@@ -134,6 +134,12 @@ enum TDX_MODULE_STATE {
 /* TODO: export the state via sysfs. */
 static enum TDX_MODULE_STATE tdx_module_state __ro_after_init;
 
+bool is_debug_seamcall_available __ro_after_init = true;
+EXPORT_SYMBOL_GPL(is_debug_seamcall_available);
+
+bool is_nonarch_seamcall_available __ro_after_init = true;
+EXPORT_SYMBOL_GPL(is_nonarch_seamcall_available);
+
 /* TDX system information returned by TDH_SYS_INFO. */
 static struct tdsysinfo_struct *tdx_tdsysinfo;
 
@@ -983,14 +989,20 @@ static int __init __tdx_init_module(void)
 		goto out;
 
 	/*
-	 * Tracing is on by default, disable it before INITTDMR which causes
-	 * too many debug messages to take long time.
+	 * Detect if debug and non-arch seamcall available.
+	 *
+	 * Even though tracing level is ALL level by default, it needs to set
+	 * it explicitly to check if debug seamcall available.
 	 */
-	if (!trace_boot_seamcalls) {
-		ret = tdh_trace_seamcalls(DEBUGCONFIG_TRACE_CUSTOM);
-		if (ret)
-			pr_err("failed to set trace level to DEBUGCONFIG_TRACE_CUSTOM.\n");
-	}
+	if (trace_boot_seamcalls)
+		tdh_trace_seamcalls(DEBUGCONFIG_TRACE_ALL);
+	else
+		/*
+		 * Tracing is on by default, disable it before INITTDMR which
+		 * causes too many debug messages to take long time.
+		 */
+		tdh_trace_seamcalls(DEBUGCONFIG_TRACE_CUSTOM);
+	tdxmode(false, 0);
 
 	ret = tdx_init_tdmrs();
 out: