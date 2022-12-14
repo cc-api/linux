a6410f48565bfc391bc8035848db3902804c29ec
Balasubramani Vivekanandan <balasubramani.vivekanandan@intel.com>
2022-12-14 11:07:33 +0530

    drm/xe/sim: Add parameter to force presi mode
    A new module parameter is introduced to force the execution environment.
    By default the parameter selects silicon mode.
    Macros created to be used in other parts of the code to check if running
    on pre-silicon.
    
    i915 reference: (7a229dc8d07d5 INTEL_DII: drm/i915/sim: Presilicon detection)
    
    Signed-off-by: Balasubramani Vivekanandan <balasubramani.vivekanandan@intel.com>
    Reviewed-by: Lucas De Marchi <lucas.demarchi@intel.com>

---
+++ drivers/gpu/drm/xe/presi/xe_presi.h
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+/*
+ * Copyright © 2022 Intel Corporation
+ */
+
+#ifndef _XE_PRESI_H_
+#define _XE_PRESI_H_
+
+#include "xe_macros.h"
+
+struct xe_device;
+
+/*
+ * We support different pre-silicon modes:
+ * - simulation: GPU is simulated. Model is functionally accurate but
+ * 		 implementation does not necessarily match HW.
+ * - emulation pipeGT: GT RTL is booted on FPGA, while the rest of the HW
+ * 		       is simulated.
+ * - emulation pipe2D: Display and Gunit RTL is booted on FPGA, while the rest
+ * 		       of the HW is simulated.
+ *
+ * Note: the enum values for detected envs are equal to the modparam values + 1
+ */
+struct xe_presi_info {
+	enum xe_presi_mode {
+		XE_PRESI_MODE_UNKNOWN = 0, /* aka not detected yet */
+		XE_PRESI_MODE_NONE = 1, /* aka SILICON */
+		XE_PRESI_MODE_SIMULATOR = 2,
+		XE_PRESI_MODE_EMULATOR_PIPEGT = 3,
+		XE_PRESI_MODE_EMULATOR_PIPE2D = 4,
+		XE_MAX_PRESI_MODE = XE_PRESI_MODE_EMULATOR_PIPE2D
+	} mode;
+};
+
+#define MODPARAM_TO_PRESI_MODE(x) ({ \
+	int val__ = (x); \
+	val__ >= 0 ? val__ + 1 : val__; \
+})
+
+#define IS_PRESI_MODE(xe, x) ({ \
+	XE_WARN_ON(xe->presi_info.mode == XE_PRESI_MODE_UNKNOWN); \
+	xe->presi_info.mode == XE_PRESI_MODE_##x; \
+})
+
+#define IS_PRESILICON(xe) (!IS_PRESI_MODE(xe, NONE))
+#define IS_SIMULATOR(xe) (IS_PRESI_MODE(xe, SIMULATOR))
+#define IS_PIPEGT_EMULATOR(xe) (IS_PRESI_MODE(xe, EMULATOR_PIPEGT))
+#define IS_PIPE2D_EMULATOR(xe) (IS_PRESI_MODE(xe, EMULATOR_PIPE2D))
+#define IS_EMULATOR(xe) (IS_PIPEGT_EMULATOR(xe) || IS_PIPE2D_EMULATOR(xe))
+
+void xe_presi_init(struct xe_device *xe);
+
+#endif
--- drivers/gpu/drm/xe/xe_device.c
+++ drivers/gpu/drm/xe/xe_device.c
@@ -38,6 +38,8 @@
 #include "xe_wait_user_fence.h"
 #include "xe_hwmon.h"
 
+#include "presi/xe_presi.h"
+
 #ifdef CONFIG_LOCKDEP
 struct lockdep_map xe_device_mem_access_lockdep_map = {
 	.name = "xe_device_mem_access_lockdep_map"
