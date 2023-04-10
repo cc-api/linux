2f7c49f9d6bdcdc88ed2d78350df908017719a7a
Haridhar Kalvala <haridhar.kalvala@intel.com>
2023-04-10 11:59:57 +0530

    drm/xe/ptl: Add PTL platform definition
    PTL is an integrated GPU based on the Xe3 architecture.
    
    Bspec: 72574
    Signed-off-by: Haridhar Kalvala <haridhar.kalvala@intel.com>
    Reviewed-by: Matt Roper <matthew.d.roper@intel.com>

--- drivers/gpu/drm/xe/xe_pci.c
+++ drivers/gpu/drm/xe/xe_pci.c
@@ -394,6 +394,12 @@ static const struct xe_device_desc bmg_desc = {
 	},
 };
 
+static const struct xe_device_desc ptl_desc = {
+	PLATFORM(XE_PANTHERLAKE),
+	.has_display = false, /* TODO: not implemented yet */
+	.require_force_probe = true,
+};
+
 #undef PLATFORM
 __diag_pop();
 
@@ -443,6 +449,7 @@ static const struct pci_device_id pciidlist[] = {
 	XE_MTL_IDS(INTEL_VGA_DEVICE, &mtl_desc),
 	XE_LNL_IDS(INTEL_VGA_DEVICE, &lnl_desc),
 	XE_BMG_IDS(INTEL_VGA_DEVICE, &bmg_desc),
+	XE_PTL_IDS(INTEL_VGA_DEVICE, &ptl_desc),
 	{ }
 };
 MODULE_DEVICE_TABLE(pci, pciidlist);
