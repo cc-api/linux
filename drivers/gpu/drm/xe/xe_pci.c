18ccf9256696e7152a9f8b28a6c84942219b51ec
Haridhar Kalvala <haridhar.kalvala@intel.com>
2023-04-10 11:59:57 +0530

    drm/xe/ptl: Add PCI IDs of PTL
    Add PCI IDs of Pather lake platform.
    
    Bspec: 72574
    
    Signed-off-by: Haridhar Kalvala <haridhar.kalvala@intel.com>
    Reviewed-by: Matt Roper <matthew.d.roper@intel.com>

--- drivers/gpu/drm/xe/xe_pci.c
+++ drivers/gpu/drm/xe/xe_pci.c
@@ -362,6 +362,12 @@ static const struct xe_device_desc lnl_desc = {
 	.require_force_probe = true,
 };
 
+static const struct xe_device_desc ptl_desc = {
+	PLATFORM(XE_PANTHERLAKE),
+	.has_4tile = true,
+	.require_force_probe = true,
+};
+
 #undef PLATFORM
 __diag_pop();
 
@@ -407,6 +413,7 @@ static const struct pci_device_id pciidlist[] = {
 	XE_MTL_IDS(INTEL_VGA_DEVICE, &mtl_desc),
 	XE_BMG_IDS(INTEL_VGA_DEVICE, &bmg_desc),
 	XE_LNL_IDS(INTEL_VGA_DEVICE, &lnl_desc),
+	XE_PTL_IDS(INTEL_VGA_DEVICE, &ptl_desc),
 	{ }
 };
 MODULE_DEVICE_TABLE(pci, pciidlist);
