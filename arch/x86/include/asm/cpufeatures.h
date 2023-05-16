d395d16f1aaf474885da0d4ebc5bb7d7cd7f472b
Kai Huang <kai.huang@intel.com>
2023-05-16 14:13:22 +1200

    x86/cpu: Detect TDX partial write machine check erratum
    TDX memory has integrity and confidentiality protections.  Violations of
    this integrity protection are supposed to only affect TDX operations and
    are never supposed to affect the host kernel itself.  In other words,
    the host kernel should never, itself, see machine checks induced by the
    TDX integrity hardware.
    
    Alas, the first few generations of TDX hardware have an erratum.  A
    partial write to a TDX private memory cacheline will silently "poison"
    the line.  Subsequent reads will consume the poison and generate a
    machine check.  According to the TDX hardware spec, neither of these
    things should have happened.
    
    Virtually all kernel memory accesses operations happen in full
    cachelines.  In practice, writing a "byte" of memory usually reads a 64
    byte cacheline of memory, modifies it, then writes the whole line back.
    Those operations do not trigger this problem.
    
    This problem is triggered by "partial" writes where a write transaction
    of less than cacheline lands at the memory controller.  The CPU does
    these via non-temporal write instructions (like MOVNTI), or through
    UC/WC memory mappings.  The issue can also be triggered away from the
    CPU by devices doing partial writes via DMA.
    
    With this erratum, there are additional things need to be done.  Similar
    to other CPU bugs, use a CPU bug bit to indicate this erratum, and
    detect this erratum during early boot.  Note this bug reflects the
    hardware thus it is detected regardless of whether the kernel is built
    with TDX support or not.
    
    Signed-off-by: Kai Huang <kai.huang@intel.com>
    Reviewed-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -488,5 +488,6 @@
 #define X86_BUG_EIBRS_PBRSB		X86_BUG(28) /* EIBRS is vulnerable to Post Barrier RSB Predictions */
 #define X86_BUG_SMT_RSB			X86_BUG(29) /* CPU is vulnerable to Cross-Thread Return Address Predictions */
 #define X86_BUG_GDS			X86_BUG(30) /* CPU is affected by Gather Data Sampling */
+#define X86_BUG_TDX_PW_MCE		X86_BUG(31) /* CPU may incur #MC if non-TD software does partial write to TDX private memory */
 
 #endif /* _ASM_X86_CPUFEATURES_H */
