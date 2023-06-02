ac13cd3ceaea7358bac818b2a9c25e61d09133db
Wei Wang <wei.w.wang@intel.com>
2023-06-02 10:48:08 +0800

    KVM: TDX: remove TDX_OPERAND_BUSY check from seamcall callers
    TODO: consider to merge it into the previous patch
    
    Signed-off-by: Wei Wang <wei.w.wang@intel.com>

--- arch/x86/kvm/vmx/tdx.c
+++ arch/x86/kvm/vmx/tdx.c
@@ -261,15 +261,7 @@ static int tdx_reclaim_page(hpa_t pa, enum pg_level level,
 	struct tdx_module_output out;
 	u64 err;
 
-	do {
-		err = tdh_phymem_page_reclaim(pa, &out);
-		/*
-		 * TDH.PHYMEM.PAGE.RECLAIM is allowed only when TD is shutdown.
-		 * state.  i.e. destructing TD.
-		 * TDH.PHYMEM.PAGE.RECLAIM requires TDR and target page.
-		 * Because we're destructing TD, it's rare to contend with TDR.
-		 */
-	} while (unlikely(err == (TDX_OPERAND_BUSY | TDX_OPERAND_ID_RCX)));
+	err = tdh_phymem_page_reclaim(pa, &out);
 	if (WARN_ON_ONCE(err)) {
 		pr_err("%s:%d:%s pa 0x%llx level %d hkid 0x%x do_wb %d\n",
 		       __FILE__, __LINE__, __func__,
@@ -1902,15 +1894,7 @@ static int tdx_sept_drop_private_spte(struct kvm *kvm, gfn_t gfn,
 
 	for (i = 0; i < KVM_PAGES_PER_HPAGE(level); i++) {
 		hpa_with_hkid = set_hkid_to_hpa(hpa, (u16)kvm_tdx->hkid);
-		do {
-			/*
-			 * TDX_OPERAND_BUSY can happen on locking PAMT entry.
-			 * Because this page was removed above, other thread
-			 * shouldn't be repeatedly operating on this page.
-			 * Simple retry should work.
-			 */
-			err = tdh_phymem_page_wbinvd(hpa_with_hkid);
-		} while (unlikely(err == (TDX_OPERAND_BUSY | TDX_OPERAND_ID_RCX)));
+		err = tdh_phymem_page_wbinvd(hpa_with_hkid);
 		if (KVM_BUG_ON(err, kvm)) {
 			pr_tdx_error(TDH_PHYMEM_PAGE_WBINVD, err, NULL);
 			r = -EIO;
@@ -2026,10 +2010,8 @@ static int tdx_sept_merge_private_spt(struct kvm *kvm, gfn_t gfn,
 	 * TDH.MEM.PAGE.PROMOTE frees the Secure-EPT page for the lower level.
 	 * Flush cache for reuse.
 	 */
-	do {
-		err = tdh_phymem_page_wbinvd(set_hkid_to_hpa(__pa(private_spt),
-							     to_kvm_tdx(kvm)->hkid));
-	} while (unlikely(err == (TDX_OPERAND_BUSY | TDX_OPERAND_ID_RCX)));
+	err = tdh_phymem_page_wbinvd(set_hkid_to_hpa(__pa(private_spt),
+                                 to_kvm_tdx(kvm)->hkid));
 	if (WARN_ON_ONCE(err)) {
 		pr_tdx_error(TDH_PHYMEM_PAGE_WBINVD, err, NULL);
 		trace_kvm_tdx_page_promote(kvm_tdx->tdr_pa, gfn,
@@ -2104,13 +2086,11 @@ static void tdx_track(struct kvm_tdx *kvm_tdx)
 	 */
 	kvm_make_all_cpus_request(&kvm_tdx->kvm, KVM_REQ_TLB_FLUSH);
 
-	do {
-		/*
-		 * kvm_flush_remote_tlbs() doesn't allow to return error and
-		 * retry.
-		 */
-		err = tdh_mem_track(kvm_tdx->tdr_pa);
-	} while (unlikely(seamcall_masked_status(err) == TDX_OPERAND_BUSY));
+	/*
+	 * kvm_flush_remote_tlbs() doesn't allow to return error and
+	 * retry.
+	 */
+	err = tdh_mem_track(kvm_tdx->tdr_pa);
 
 	/* Release remote vcpu waiting for TDH.MEM.TRACK in tdx_flush_tlb(). */
 	atomic_dec(&kvm_tdx->tdh_mem_track);
