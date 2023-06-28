5c4e5125ca53ddc163a04895aa67f2a56c761c22
Yang Zhong <yang.zhong@linux.intel.com>
2023-06-28 08:32:14 -0400

    KVM: VMX: Add apic_timer_virt module_param
    KVM disable guest apic timer by default, we can enable this timer by
    new apic_timer_virt module_param.
    
    If guest changes APIC_ID or APIC_BASE, KVM may disable APICv, so kvm
    vmx_refresh_apicv_exec_ctrl() need clear guest timer bit Once APICv
    disabled.
    
    Signed-off-by: Yang Zhong <yang.zhong@linux.intel.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -108,6 +108,9 @@ module_param(enable_apicv, bool, S_IRUGO);
 bool __read_mostly enable_ipiv = true;
 module_param(enable_ipiv, bool, 0444);
 
+bool __read_mostly enable_apic_timer_virt;
+module_param_named(apic_timer_virt, enable_apic_timer_virt, bool, 0444);
+
 /*
  * If nested=1, nested virtualization is supported, i.e., guests may use
  * VMX and be a hypervisor for its own guests. If nested=0, guests may not
@@ -4457,6 +4460,9 @@ static void vmx_refresh_apicv_exec_ctrl(struct kvm_vcpu *vcpu)
 						 SECONDARY_EXEC_VIRTUAL_INTR_DELIVERY);
 		if (enable_ipiv)
 			tertiary_exec_controls_clearbit(vmx, TERTIARY_EXEC_IPI_VIRT);
+
+		if(enable_apic_timer_virt)
+			tertiary_exec_controls_clearbit(vmx, TERTIARY_EXEC_GUEST_APIC_TIMER);
 	}
 
 	vmx_update_msr_bitmap_x2apic(vcpu);
@@ -4516,6 +4522,11 @@ static u64 vmx_tertiary_exec_control(struct vcpu_vmx *vmx)
 	 */
 	if (!enable_ipiv || !kvm_vcpu_apicv_active(&vmx->vcpu))
 		exec_control &= ~TERTIARY_EXEC_IPI_VIRT;
+
+	/* GUEST_APIC_TIMER is enabled when guest enable lapic timer as tsc
+	 * deadline mode.
+	 */
+	exec_control &= ~TERTIARY_EXEC_GUEST_APIC_TIMER;
 
 	return exec_control;
 }
@@ -7392,7 +7403,8 @@ static fastpath_t vmx_vcpu_run(struct kvm_vcpu *vcpu)
 	if (enable_preemption_timer)
 		vmx_update_hv_timer(vcpu);
 
-	vmx_update_guest_virt_timer(vcpu);
+	if (enable_apic_timer_virt)
+		vmx_update_guest_virt_timer(vcpu);
 
 	kvm_wait_lapic_expire(vcpu);
 
@@ -8110,7 +8122,8 @@ static int vmx_set_guest_virt_timer(struct kvm_vcpu *vcpu, u16 vector)
 	struct vcpu_vmx *vmx;
 
 	vmx = to_vmx(vcpu);
-	if (!kvm_vcpu_apicv_active(vcpu) ||
+	if (!enable_apic_timer_virt ||
+	    !kvm_vcpu_apicv_active(vcpu) ||
 	    (exec_controls_get(vmx) & CPU_BASED_RDTSC_EXITING))
 		return -EPERM;
 
@@ -8617,6 +8630,9 @@ static __init int hardware_setup(void)
 
 	if (!cpu_has_vmx_preemption_timer())
 		enable_preemption_timer = false;
+
+	if (!cpu_has_vmx_guest_virt_timer())
+		enable_apic_timer_virt = false;
 
 	if (enable_preemption_timer) {
 		u64 use_timer_freq = 5000ULL * 1000 * 1000;
