5975c5a4af61c98e5ceb614082299fbc3bbd5bf5
Moti Haimovski <mhaimovski@habana.ai>
2023-06-30 09:30:33 +0300

    drm/xe/presi/fs1: add PLDM mode to xe_presi_mode
    Add support for configuring the pre-silicon (presi) driver
    to run on palladium.
    
    Signed-off-by: Moti Haimovski <mhaimovski@habana.ai>

--- drivers/gpu/drm/xe/presi/xe_presi.c
+++ drivers/gpu/drm/xe/presi/xe_presi.c
@@ -16,13 +16,13 @@ static const char * const presi_mode_names[] = {
 	[XE_PRESI_MODE_SIMULATOR] = "simulation",
 	[XE_PRESI_MODE_EMULATOR_PIPEGT] = "emulation pipeGT",
 	[XE_PRESI_MODE_EMULATOR_PIPE2D] = "emulation pipe2D",
+	[XE_PRESI_MODE_EMULATOR_PLDM] = "emulation palladium",
 };
 
 static int xe_presi_mode = 0;
 module_param_named_unsafe(presi_mode, xe_presi_mode, int, 0600);
-MODULE_PARM_DESC(presi_mode, "Select pre-si mode "
-		 "(0=none/silicon [default], 1=simulator,"
-		 "2=pipeGT emulator, 3=pipe2D emulator)");
+MODULE_PARM_DESC(presi_mode,
+		 "Select pre-si mode (0=none/silicon [default], 1=simulator, 2=pipeGT emulator, 3=pipe2D emulator, 4=palladium emulator)");
 
 static int xe_presi_timeout_multiplier = 0;
 module_param_named_unsafe(presi_timeout_multiplier,
@@ -124,7 +124,7 @@ void xe_presi_init(struct xe_device *xe)
 	BUILD_BUG_ON(XE_PRESI_MODE_UNKNOWN); /* unknown needs to be 0 */
 	XE_WARN_ON(xe->presi_info.mode != XE_PRESI_MODE_UNKNOWN);
 
-	if (mode > XE_PRESI_MODE_NONE && mode <= XE_MAX_PRESI_MODE) {
+	if (mode > XE_PRESI_MODE_NONE && mode < XE_NUM_PRESI_MODES) {
 		drm_info(&xe->drm, "using pre-silicon mode from modparam: %s\n",
 				 presi_mode_names[mode]);
 		xe->presi_info.mode = mode;
