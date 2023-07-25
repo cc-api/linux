254c7cdcbc8b811ad8ace3aa475838703b4b7048
Balasubramani Vivekanandan <balasubramani.vivekanandan@intel.com>
2023-07-25 19:03:03 +0530

    drm/xe/sim: Dummy XE_PRESI_SKIP_FEATURE define for i915
    Dummy macro defined for XE_PRESI_SKIP_FEATURE to enable building i915
    when the following commits makes use of the macro.
    
    Signed-off-by: Balasubramani Vivekanandan <balasubramani.vivekanandan@intel.com>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -419,6 +419,7 @@ static inline struct intel_gt *to_gt(struct drm_i915_private *i915)
 
 #define IS_SIMULATOR(i915) (0)
 #define IS_PRESILICON(i915) (0)
+#define XE_PRESI_SKIP_FEATURE(i915, feature) (0)
 
 #define INTEL_INFO(i915)	(&(i915)->__info)
 #define DISPLAY_INFO(i915)	(INTEL_INFO(i915)->display)
