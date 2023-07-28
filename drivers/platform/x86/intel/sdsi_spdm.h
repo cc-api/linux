bd9d981a2be17bf2c91f0e9bc0adc5da59cad7cb
David E. Box <david.e.box@linux.intel.com>
2023-07-28 12:48:47 -0700

    platform/x86/intel/sdsi_spdm: Add support for GET_MEASUREMENTS
    Add support for doing GET_MEASUREMENTS. This allows retrieval of firmware
    measurements (e.g. hash of ROM) after the device has been pre-authenticated
    through attestation. Per the spec, measurement requests can happen multiple
    times.  If signing is supported, a transcript of all of the measurement
    requests will be taken and an incremental hash computed. When a signing
    request is made, finalize the hash and use it to verify the signature
    returned by the device (Responder). If signing is supported, assume that a
    signed request can be made at any time and start calculating the hash. Per
    the spec, end the transcript after,
    
    	- a signed measurement is requested,
    	- a message other than GET_MEASUREMENTS is sent,
    	- an error occurs.
    
    The leaf key obtained during spdm_authorize will be used to verify the
    signature. Use a callback to send each measurement back to the caller after
    is has be obtained.
    
    Signed-off-by: David E. Box <david.e.box@linux.intel.com>

--- drivers/platform/x86/intel/sdsi_spdm.h
+++ drivers/platform/x86/intel/sdsi_spdm.h
@@ -18,6 +18,31 @@ struct key;
 struct device;
 struct sdsi_spdm_state;
 
+enum measurement_op {
+	MEASUREMENT_OP_QUERY,
+	MEASUREMENT_OP_ONE,
+	MEASUREMENT_OP_ALL,
+};
+
+#define MEASUREMENT_ATTR_SIGN	BIT(0)
+#define MEASUREMENT_ATTR_RAW	BIT(1)
+
+struct spdm_measurement_block {
+	u8 index;
+	u8 specification;
+	u16 size;
+	/* Variable measurement size up to 64k */
+};
+
+struct spdm_measurement_request {
+	enum measurement_op op;
+	u8 attribute;
+	u8 block_no;
+	u8 slot_id;
+	void *priv;
+	void (*meas_cb)(u8 *measurement, size_t count, void *priv);
+};
+
 typedef int (sdsi_spdm_transport)(void *priv, struct device *dev,
 			     const void *request, size_t request_sz,
 			     void *response, size_t response_sz);
@@ -27,6 +52,9 @@ struct sdsi_spdm_state *sdsi_spdm_create(struct device *dev, sdsi_spdm_transport
 			       struct key *keyring);
 
 int sdsi_spdm_authenticate(struct sdsi_spdm_state *spdm_state);
+
+int sdsi_spdm_get_measurements(struct sdsi_spdm_state *spdm_state,
+			  struct spdm_measurement_request *m);
 
 void sdsi_spdm_destroy(struct sdsi_spdm_state *spdm_state);
 
