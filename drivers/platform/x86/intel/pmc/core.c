afd2cf8b86c1a1a193c76894d05b7fed621e5455
Rajvi Jingar <rajvi.jingar@linux.intel.com>
2023-08-02 15:22:08 -0700

    platform/x86: intel/pmc/: Add Arrow Lake-S support to pmc core driver
    Signed-off-by: Rajvi Jingar <rajvi.jingar@linux.intel.com>

--- drivers/platform/x86/intel/pmc/core.c
+++ drivers/platform/x86/intel/pmc/core.c
@@ -1236,8 +1236,10 @@ static const struct x86_cpu_id intel_pmc_core_ids[] = {
 	X86_MATCH_INTEL_FAM6_MODEL(METEORLAKE_L,	mtl_l_core_init),
 	X86_MATCH_INTEL_FAM6_MODEL(METEORLAKE,		mtl_core_init),
 	X86_MATCH_INTEL_FAM6_MODEL(LAKEFIELD,		lkf_core_init),
+	X86_MATCH_INTEL_FAM6_MODEL(ARROWLAKE,		mtl_core_init),
 	{}
 };
+
 
 MODULE_DEVICE_TABLE(x86cpu, intel_pmc_core_ids);
 
