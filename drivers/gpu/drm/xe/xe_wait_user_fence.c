3d7ae7ea69f4425d0bc6f7a26177bbdd51a83aae
Lucas De Marchi <lucas.demarchi@intel.com>
2023-08-03 12:44:01 -0700

    drm/xe/sim: Use additional timeout when waiting for user fence
    On slower simulation we hit some failures due to timeout passed by igt.
    Just use the same multiplier and wait more.
    
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>

--- drivers/gpu/drm/xe/xe_wait_user_fence.c
+++ drivers/gpu/drm/xe/xe_wait_user_fence.c
@@ -15,6 +15,8 @@
 #include "xe_macros.h"
 #include "xe_vm.h"
 
+#include "presi/xe_presi.h"
+
 static int do_compare(u64 addr, u64 value, u64 mask, u16 op)
 {
 	u64 rvalue;
@@ -198,6 +200,7 @@ int xe_wait_user_fence_ioctl(struct drm_device *dev, void *data,
 	}
 
 	timeout = to_jiffies_timeout(xe, args);
+	timeout *= XE_PRESI_TIMEOUT_MULTIPLIER(xe);
 
 	start = ktime_get();
 
