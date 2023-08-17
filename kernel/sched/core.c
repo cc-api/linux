mateusz-mowka/tboot_linux

sched/core: Expose __migrate_swap_task() outside of NUMA balancing
The NUMA balancer provides functionality to swap the CPUs of two tasks.
This functionality is needed by the Intel Thread Director technology
when it detects that swapping tasks can increace throughput. Expose it.

Signed-off-by: Ricardo Neri <ricardo.neri-calderon@linux.intel.com>

kernel/sched/core.c
@@ -3140,8 +3140,8 @@ void set_task_cpu(struct task_struct *p, unsigned int new_cpu)
 	__set_task_cpu(p, new_cpu);
 }
 
-#ifdef CONFIG_NUMA_BALANCING
-static void __migrate_swap_task(struct task_struct *p, int cpu)
+#if defined(CONFIG_SCHED_TASK_CLASSES) || defined(CONFIG_NUMA_BALANCING)
+void __migrate_swap_task(struct task_struct *p, int cpu)
 {
 	if (task_on_rq_queued(p)) {
 		struct rq *src_rq, *dst_rq;
@@ -3170,12 +3170,14 @@ static void __migrate_swap_task(struct task_struct *p, int cpu)
 		p->wake_cpu = cpu;
 	}
 }
+#endif
 
 struct migration_swap_arg {
 	struct task_struct *src_task, *dst_task;
 	int src_cpu, dst_cpu;
 };
 
+#ifdef CONFIG_NUMA_BALANCING
 static int migrate_swap_stop(void *data)
 {
 	struct migration_swap_arg *arg = data;

kernel/sched/sched.h
@@ -1667,6 +1667,10 @@ static inline int sched_numa_find_closest(const struct cpumask *cpus, int cpu)
 }
 #endif
 
+#if defined(CONFIG_SCHED_TASK_CLASSES) || defined(CONFIG_NUMA_BALANCING)
+void __migrate_swap_task(struct task_struct *p, int cpu);
+#endif
+
 #ifdef CONFIG_NUMA_BALANCING
 /* The regions in numa_faults array from task_struct */
 enum numa_faults_stats {

cc-api/linux

kernel/sched/core.c
@@ -3373,8 +3373,8 @@ void set_task_cpu(struct task_struct *p, unsigned int new_cpu)
 	__set_task_cpu(p, new_cpu);
 }
 
-#ifdef CONFIG_NUMA_BALANCING
-static void __migrate_swap_task(struct task_struct *p, int cpu)
+#if defined(CONFIG_IPC_CLASSES) || defined(CONFIG_NUMA_BALANCING)
+void __migrate_swap_task(struct task_struct *p, int cpu)
 {
 	if (task_on_rq_queued(p)) {
 		struct rq *src_rq, *dst_rq;
@@ -3403,12 +3403,14 @@ static void __migrate_swap_task(struct task_struct *p, int cpu)
 		p->wake_cpu = cpu;
 	}
 }
+#endif
 
 struct migration_swap_arg {
 	struct task_struct *src_task, *dst_task;
 	int src_cpu, dst_cpu;
 };
 
+#ifdef CONFIG_NUMA_BALANCING
 static int migrate_swap_stop(void *data)
 {
 	struct migration_swap_arg *arg = data;

kernel/sched/sched.h
@@ -1737,6 +1737,10 @@ static inline int sched_numa_find_closest(const struct cpumask *cpus, int cpu)
 }
 #endif
 
+#if defined(CONFIG_IPC_CLASSES) || defined(CONFIG_NUMA_BALANCING)
+void __migrate_swap_task(struct task_struct *p, int cpu);
+#endif
+
 #ifdef CONFIG_NUMA_BALANCING
 /* The regions in numa_faults array from task_struct */
 enum numa_faults_stats {