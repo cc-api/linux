6a3b592d0d2c5854a591042e0579bb435575e371
Matt Roper <matthew.d.roper@intel.com>
2023-08-17 16:55:55 -0700

    drm/xe/sim: Add pre-si "concept" device ID
    Pre-silicon teams plan to use device ID 0xB0FF to test IP changes for
    future platforms.  This device ID should identify as a Panther Lake
    platform, but the pre-silicon models using this ID may include features
    and behavior from future platforms (e.g., Xe3p).
    
    HSD: 22018682945
    Signed-off-by: Matt Roper <matthew.d.roper@intel.com>
    Reviewed-by: Gustavo Sousa <gustavo.sousa@intel.com>

--- include/drm/xe_pciids.h
+++ include/drm/xe_pciids.h
@@ -242,7 +242,8 @@
 
 #define XE_PTL_IDS(MACRO__, ...) \
 	XE_PTL_P_IDS(MACRO__, ## __VA_ARGS__), \
-	XE_PTL_U_IDS(MACRO__, ## __VA_ARGS__)
+	XE_PTL_U_IDS(MACRO__, ## __VA_ARGS__), \
+	MACRO__(0xB0FF, ## __VA_ARGS__)		/* Pre-si only */
 
 #define XE_FS1_IDS(MACRO__, ...) \
 	MACRO__(0x0B73, ## __VA_ARGS__)
