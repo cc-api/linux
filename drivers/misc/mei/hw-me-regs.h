561aaabc552828b2049007b241bf346d24413b66
Alexander Usyskin <alexander.usyskin@intel.com>
2023-09-12 10:50:05 +0300

    mei: me: [EMBARGO] add nova lake point P DID
    Add Nova Lake P device id.
    
    Signed-off-by: Alexander Usyskin <alexander.usyskin@intel.com>

--- drivers/misc/mei/hw-me-regs.h
+++ drivers/misc/mei/hw-me-regs.h
@@ -120,6 +120,7 @@
 #define MEI_DEV_ID_PTL_P      0xE470  /* Panther Lake P */
 
 #define MEI_DEV_ID_NVL_S      0x6E68  /* Nova Lake Point S */
+#define MEI_DEV_ID_NVL_P      0xD270  /* Nova Lake Point P */
 
 /*
  * MEI HW Section
