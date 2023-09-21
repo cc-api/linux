mateusz-mowka/tboot_linux

DEBUG: proc: Add a /proc/<pid>/classid_raw
classid_raw provides the last known class of <pid> as provided by
hardware.

Add convenience infrastructure to query the raw class of a given task.
This is useful to monitor the result of hardware classification of tasks.

Signed-off-by: Ricardo Neri <ricardo.neri-calderon@linux.intel.com>

fs/proc/base.c
@@ -377,6 +377,32 @@ static const struct file_operations proc_pid_cmdline_ops = {
 	.llseek	= generic_file_llseek,
 };
 
+static ssize_t proc_pid_class_raw_read(struct file *file, char __user *buf,
+				       size_t count, loff_t *pos)
+{
+	struct task_struct *tsk;
+	char buffer[PROC_NUMBUF];
+	int class_raw;
+	ssize_t len;
+
+	tsk = get_proc_task(file_inode(file));
+	if (!tsk)
+		return -ESRCH;
+
+#ifdef CONFIG_SCHED_TASK_CLASSES
+	class_raw = tsk->class_raw;
+#else
+	class_raw = TASK_CLASS_UNCLASSIFIED;
+#endif
+	len = snprintf(buffer, sizeof(buffer), "%d\n", class_raw);
+	put_task_struct(tsk);
+	return simple_read_from_buffer(buf, count, pos, buffer, len);
+}
+
+static const struct file_operations proc_pid_class_raw_ops = {
+	.read	= proc_pid_class_raw_read,
+};
+
 #ifdef CONFIG_KALLSYMS
 /*
  * Provides a wchan file via kallsyms in a proper one-value-per-file format.
@@ -3219,6 +3245,7 @@ static const struct pid_entry tgid_base_stuff[] = {
 	ONE("syscall",    S_IRUSR, proc_pid_syscall),
 #endif
 	REG("cmdline",    S_IRUGO, proc_pid_cmdline_ops),
+	REG("classid_raw", S_IRUGO, proc_pid_class_raw_ops),
 	ONE("stat",       S_IRUGO, proc_tgid_stat),
 	ONE("statm",      S_IRUGO, proc_pid_statm),
 	REG("maps",       S_IRUGO, proc_pid_maps_operations),
@@ -3563,6 +3590,7 @@ static const struct pid_entry tid_base_stuff[] = {
 	ONE("syscall",   S_IRUSR, proc_pid_syscall),
 #endif
 	REG("cmdline",   S_IRUGO, proc_pid_cmdline_ops),
+	REG("classid_raw", S_IRUGO, proc_pid_class_raw_ops),
 	ONE("stat",      S_IRUGO, proc_tid_stat),
 	ONE("statm",     S_IRUGO, proc_pid_statm),
 	REG("maps",      S_IRUGO, proc_pid_maps_operations),

cc-api/linux

fs/proc/base.c
@@ -378,6 +378,35 @@ static const struct file_operations proc_pid_cmdline_ops = {
 	.llseek	= generic_file_llseek,
 };
 
+#ifdef CONFIG_SCHED_DEBUG
+static ssize_t proc_pid_class_raw_read(struct file *file, char __user *buf,
+				       size_t count, loff_t *pos)
+{
+	struct task_struct *tsk;
+	char buffer[PROC_NUMBUF];
+	int class_raw;
+	ssize_t len;
+
+	tsk = get_proc_task(file_inode(file));
+	if (!tsk)
+		return -ESRCH;
+
+#ifdef CONFIG_IPC_CLASSES
+	class_raw = tsk->ipcc_raw;
+#else
+	class_raw = IPC_CLASS_UNCLASSIFIED;
+#endif
+	len = snprintf(buffer, sizeof(buffer), "%d\n", class_raw);
+	put_task_struct(tsk);
+	return simple_read_from_buffer(buf, count, pos, buffer, len);
+}
+
+static const struct file_operations proc_pid_class_raw_ops = {
+	.read	= proc_pid_class_raw_read,
+	.llseek	= generic_file_llseek,
+};
+#endif
+
 #ifdef CONFIG_KALLSYMS
 /*
  * Provides a wchan file via kallsyms in a proper one-value-per-file format.
@@ -3266,6 +3295,9 @@ static const struct pid_entry tgid_base_stuff[] = {
 	ONE("syscall",    S_IRUSR, proc_pid_syscall),
 #endif
 	REG("cmdline",    S_IRUGO, proc_pid_cmdline_ops),
+#ifdef CONFIG_SCHED_DEBUG
+	REG("classid_raw", S_IRUGO, proc_pid_class_raw_ops),
+#endif
 	ONE("stat",       S_IRUGO, proc_tgid_stat),
 	ONE("statm",      S_IRUGO, proc_pid_statm),
 	REG("maps",       S_IRUGO, proc_pid_maps_operations),
@@ -3613,6 +3645,9 @@ static const struct pid_entry tid_base_stuff[] = {
 	ONE("syscall",   S_IRUSR, proc_pid_syscall),
 #endif
 	REG("cmdline",   S_IRUGO, proc_pid_cmdline_ops),
+#ifdef CONFIG_SCHED_DEBUG
+	REG("classid_raw", S_IRUGO, proc_pid_class_raw_ops),
+#endif
 	ONE("stat",      S_IRUGO, proc_tid_stat),
 	ONE("statm",     S_IRUGO, proc_pid_statm),
 	REG("maps",      S_IRUGO, proc_pid_maps_operations),