mateusz-mowka/tboot_linux

sched/fair: Let a busy CPU balance load for classes of tasks
On hybrid processors that support classes of tasks, there exist
differences in the instructions-per-cycle properties of CPUs.

In such cases, throughput may be increased if the destination CPU swaps a
task in its runqueue with a task from another CPU for which it has a
higher instructions-per-cycle ratio.

Let a busy CPU proceed with load balancing to identify tasks it can swap.

Signed-off-by: Ricardo Neri <ricardo.neri-calderon@linux.intel.com>

kernel/sched/fair.c
@@ -10010,6 +10010,14 @@ static int should_we_balance(struct lb_env *env)
 		return 0;
 
 	/*
+	 * If classes of tasks are supported, a busy CPU may decide to pull a
+	 * task from the busiest queue if doing so increaes throughput due to
+	 * differences in instructions-per-cycle among CPUs.
+	 */
+	if (sched_task_classes_enabled() && env->idle == CPU_NOT_IDLE)
+		return 1;
+
+	/*
 	 * In the newly idle case, we will allow all the CPUs
 	 * to do the newly idle load balance.
 	 */

cc-api/linux

kernel/sched/fair.c
@@ -11414,6 +11414,14 @@ static int should_we_balance(struct lb_env *env)
 		return 0;
 
 	/*
+	 * If classes of tasks are supported, a busy CPU may decide to pull a
+	 * task from the busiest queue if doing so increaes throughput due to
+	 * differences in instructions-per-cycle among CPUs.
+	 */
+	if (sched_ipcc_enabled() && env->idle == CPU_NOT_IDLE)
+		return 1;
+
+	/*
 	 * In the newly idle case, we will allow all the CPUs
 	 * to do the newly idle load balance.
 	 *