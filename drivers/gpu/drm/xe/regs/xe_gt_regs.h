aeefb7e267ab155f57dc5b857415fda8602df12a
Matt Roper <matthew.d.roper@intel.com>
2023-09-21 15:43:09 -0700

    drm/xe/xe3p_xpc: Obtain dss/slice from fusing
    On Xe3p, the physical number of DSS that compose a "slice" can vary by
    platform SKU (note:  this is the group size _before_ we subtract any
    fused-off DSS, not a number of DSS that are actually available/usable).
    We should now read this number from the FUSE4 register and utilize it
    when figuring out grpid and instanceid for steering purposes.
    
    Bspec: 62483
    Signed-off-by: Matt Roper <matthew.d.roper@intel.com>

--- drivers/gpu/drm/xe/regs/xe_gt_regs.h
+++ drivers/gpu/drm/xe/regs/xe_gt_regs.h
@@ -175,6 +175,7 @@
 
 /* Fuse readout registers for GT */
 #define XEHP_FUSE4				XE_REG(0x9114)
+#define   NUM_SS_PER_SLICE			REG_GENMASK(30, 26)
 #define   CCS_EN_MASK				REG_GENMASK(19, 16)
 #define   GT_L3_EXC_MASK			REG_GENMASK(6, 4)
 
