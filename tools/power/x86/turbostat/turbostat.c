9b136d052de28ec366a3a8e18d265215970e10c2
Zhang Rui <rui.zhang@intel.com>
2023-09-28 13:15:19 +0800

    tools/power/turbostat: Internal: Add support for ClearwaterForest
    Add turbostat support for ClearwaterForest.
    
    It shares the same features with SierraForest.
    
    Signed-off-by: Zhang Rui <rui.zhang@intel.com>

--- tools/power/x86/turbostat/turbostat.c
+++ tools/power/x86/turbostat/turbostat.c
@@ -885,6 +885,8 @@ static const struct platform_data turbostat_pdata[] = {
 	{ INTEL_FAM6_ATOM_GRACEMONT, &adl_features },
 	{ INTEL_FAM6_ATOM_CRESTMONT_X, &srf_features },
 	{ INTEL_FAM6_ATOM_CRESTMONT, &grr_features },
+	{ INTEL_FAM6_ATOM_DARKMONT_X, &srf_features },
+	{ INTEL_FAM6_ATOM_DARKMONT, &srf_features },
 	{ INTEL_FAM6_XEON_PHI_KNL, &knl_features },
 	{ INTEL_FAM6_XEON_PHI_KNM, &knl_features },
 	/*
