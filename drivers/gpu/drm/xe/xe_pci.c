3c4ecba58cd4ce342ecafb8377674fcbfcf86815
Ofir Bitton <obitton@habana.ai>
2023-10-01 15:04:14 +0300

    [FSW-162] drm/xe/presi/fs1: introduce presi ops
    Support the fs1 device in its pre-silicon stage by adding
    a set of pre-silicon configuration parameters and initialization
    routines which provides the ability to override the device default
    parameters and to work with only parts of the ASIC HW.
    
    Signed-off-by: Ohad Sharabi <osharabi@habana.ai>
    Signed-off-by: Moti Haimovski <mhaimovski@habana.ai>

--- drivers/gpu/drm/xe/xe_pci.c
+++ drivers/gpu/drm/xe/xe_pci.c
@@ -817,7 +817,9 @@ static int xe_pci_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 	if (err)
 		goto err_pci_disable;
 
-	xe_presi_init(xe);
+	err = xe_presi_init(xe);
+	if (err)
+		goto err_drm_put;
 
 	xe_display_info_init(xe);
 
